e0b12992bbf3ebe9f0167bdb890eee9a4721f639
teerenzo <renzahoemmanuel8@gmail.com>
2024-07-31 13:21:50 +0200

    setting up project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+        <h1>Home</h1>
+        <Footer />
+        </div>
+    );
+    };
+
+export default Home;
